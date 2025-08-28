709b4250418c76c80c1df67823a6762ee27dd33f
Turbodan2006 <danielmsantana2006@gmail.com>
2025-08-28 21:52:04 +0000

    Comentei os dados

--- war.c
+++ war.c
@@ -1,31 +1,32 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdio.h> //biblioteca padrão em C
+#include <string.h> //biblioteca de manipulação de strings em C
 
 typedef struct {
-    char nome[30];
-    char corExercito[10];
-    int numTropas;
-} Territorio;
+    char nome[30]; //Nome do jogador
+    char corExercito[10]; //Cor do exército
+    int numTropas; //Número de tropas
+} Territorio; // Uma estrutura chamada Territorio(nome,cor e tropas)
 
 int main() {
     
-    Territorio war[5];
+    Territorio war[5]; //Cria um vetor para cada jogador
 
+    //Preenche dados de cada Território
     for(int i = 0; i < 5; i++) {
         printf("Digite o nome da pessoa %d: ", i+1);
-        scanf(" %[^\n]", war[i].nome); 
+        scanf(" %[^\n]", war[i].nome); //Lê o nome do jogador
 
         printf("Digite a cor do exército %d: ", i+1);
-        scanf(" %[^\n]", war[i].corExercito);
+        scanf(" %[^\n]", war[i].corExercito);//Lê a cor do exército
 
         printf("Digite o número de tropas do exército %d: ", i+1);
-        scanf("%d", &war[i].numTropas);
+        scanf("%d", &war[i].numTropas); //Lê o número de tropas
     }
-
+    //Exibe os dados armazenados
     for(int i = 0; i < 5; i++) {
-        printf("Nome da pessoa %d: %s\n", i+1, war[i].nome);
-        printf("Cor do exército %d: %s\n", i+1, war[i].corExercito);
-        printf("Número de tropas do exército %d: %d\n\n", i+1, war[i].numTropas);
+        printf("Nome da pessoa %d: %s\n", i+1, war[i].nome); //Exibe o nome do jogador
+        printf("Cor do exército %d: %s\n", i+1, war[i].corExercito); //Exibe a cor do exército
+        printf("Número de tropas do exército %d: %d\n\n", i+1, war[i].numTropas); //Exibe o número de tropas
     }
 
     return 0;
