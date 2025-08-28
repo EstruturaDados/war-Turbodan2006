a114e00c98059517cddb042ba6602acb381ba43f
Turbodan2006 <danielmsantana2006@gmail.com>
2025-08-28 21:35:23 +0000

    Criei o vetor war e criei um vetor que pede para o usuário digitar o nome, a cor e as tropas

--- war.c
+++ war.c
@@ -9,5 +9,18 @@ typedef struct {
 
 int main() {
     
+    Territorio war[5];
+
+    for(int i = 0; i < 5; i++) {
+        printf("Digite o nome da pessoa %d: ", i+1);
+        scanf(" %[^\n]", war[i].nome); 
+
+        printf("Digite a cor do exército %d: ", i+1);
+        scanf(" %[^\n]", war[i].corExercito);
+
+        printf("Digite o número de tropas do exército %d: ", i+1);
+        scanf("%d", &war[i].numTropas);
+    }
+    
     return 0;
 }
